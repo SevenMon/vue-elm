7db2f939714453640c80182a0ddc222fe6d5f7c1
葛峰 <>
2018-04-12 13:45:58 +0800

    aaa

--- src/components/Good.vue
+++ src/components/Good.vue
@@ -22,15 +22,16 @@
 									<img :src="food.icon"/>
 								</div>
 								<div class="food-info">
-									<h1>{{ food.name }}</h1>
+									<h1 class="food-title">{{ food.name }}</h1>
 									<span class="food-des">{{ food.description }}</span>
 									<div class="food-sell-rat">
 										<span class="food_sell">月售{{ food.sellCount }}份</span>
 										<span class="food_rat">好评率{{ food.rating }}%</span>
 									</div>
 									<div class="food-price">
+										<span class="food-new-code">¥</span>
 										<span class="food-new-price">{{ food.price }}</span>
-										<span v-show="food.oldPrice" class="food-old-price">{{ food.oldPrice }}</span>
+										<span v-show="food.oldPrice" class="food-old-price">¥{{ food.oldPrice }}</span>
 									</div>
 								</div>
 							</div>
@@ -130,6 +131,7 @@
 	}
 	.good .foods-wrapper{
 		width: 100%;
+		
 	}
 	.good .foods-wrapper .foods-ul{
 		
@@ -150,14 +152,22 @@
 		
 	}
 	.good .foods-wrapper .foods-ul .foods-list .food-list-ul .food-item{
-		
+		margin: 18px;
+		border-bottom: 1px solid rgba(7,17,27,0.1);
+		padding-bottom: 18px;
+	}
+	.good .foods-wrapper .foods-ul .foods-list .food-list-ul .food-item:last-child{
+		border-bottom:none
 	}
 	.good .foods-wrapper .foods-ul .foods-list .food-list-ul .food-item .food-content{
-		margin: 18px;
 		display: flex;
 	}
 	.good .foods-wrapper .foods-ul .foods-list .food-list-ul .food-item .food-content .food-img{
-		
+		flex: 0 0 57px;
+	}
+	.good .foods-wrapper .foods-ul .foods-list .food-list-ul .food-item .food-content .food-img img{
+		width: 57px;
+		height: 57px;
 	}
 	.good .foods-wrapper .foods-ul .foods-list .food-list-ul .food-item .food-content .food-info{
 		flex: 1;
@@ -170,25 +180,45 @@
 		color: rgb(7,17,27);
 	}
 	.good .foods-wrapper .foods-ul .foods-list .food-list-ul .food-item .food-content .food-info .food-des{
-		
+		margin-top: 8px;
+		font-size: 10px;
+		color: rgb(147,153,159);
+		line-height: 10px;
+		display: inline-block;
 	}
 	.good .foods-wrapper .foods-ul .foods-list .food-list-ul .food-item .food-content .food-info .food-sell-rat{
-		
+		margin-top: 8px;
+		color: rgb(147,153,159);
+		line-height: 10px;
 	}
 	.good .foods-wrapper .foods-ul .foods-list .food-list-ul .food-item .food-content .food-info .food-sell-rat .food_sell{
-		
+		font-size: 10px;
+		margin-right: 8px;
 	}
 	.good .foods-wrapper .foods-ul .foods-list .food-list-ul .food-item .food-content .food-info .food-sell-rat .food_rat{
+		font-size: 10px;
 		
 	}
 	.good .foods-wrapper .foods-ul .foods-list .food-list-ul .food-item .food-content .food-info .food-price{
 		
 	}
+	.good .foods-wrapper .foods-ul .foods-list .food-list-ul .food-item .food-content .food-info .food-price .food-new-code{
+		font-size: 10px;
+		color: rgb(240,20,20);
+		font-weight: normal;
+		line-height: 24px;
+	}
 	.good .foods-wrapper .foods-ul .foods-list .food-list-ul .food-item .food-content .food-info .food-price .food-new-price{
-		
+		font-size: 14px;
+		color: rgb(240,20,20);
+		font-weight: 700;
+		line-height: 24px;
 	}
 	.good .foods-wrapper .foods-ul .foods-list .food-list-ul .food-item .food-content .food-info .food-price .food-old-price{
-		
+		color: rgb(147,153,159);
+		font-size: 10px;
+		font-weight: normal;
+		line-height: 24px;
 	}
 
 	
